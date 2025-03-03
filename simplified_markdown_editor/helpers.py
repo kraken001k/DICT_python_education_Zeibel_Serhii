ace0fe7fd921f1ed38fdcc73216dc88f753604de
kraken001k <s.s.zeibel@student.khai.edu>
2025-03-03 21:37:33 +0200

    SMR 1
    SME 2
    
    SME 3
    
    SME 4
    
    fix unstaged before rebase

---
+++ simplified_markdown_editor/helpers.py
@@ -0,0 +1,4 @@
+# Prints available formatters and special commands.
+def print_help():
+    print("Available formatters: plain bold italic header link inline-code ordered-list unordered-list new-line")
+    print("Special commands: !help !done")
